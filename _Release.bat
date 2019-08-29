62fd8511b3dc1baf8782c48a2324993c25462b24
stackprobe <stackprobes@gmail.com>
2019-08-29 09:12:17 +0900

    Dream

--- _Release.bat
+++ _Release.bat
@@ -12,7 +12,7 @@ COPY /B PurpleDiamond\PurpleDiamond\bin\Release\DxLibDotNet.dll out
 C:\Factory\Tools\xcp.exe doc out
 C:\Factory\Tools\xcp.exe C:\Dev\Fairy\Donut2\doc out
 
-C:\Factory\SubTools\zip.exe /G out PurpleDiamond
+C:\Factory\SubTools\zip.exe /PE- /G out PurpleDiamond
 C:\Factory\Tools\summd5.exe /M out
 
 PAUSE
