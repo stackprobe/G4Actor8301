bd6325046ac6c6c498f4d2dffcdd1cec614bb102
stackprobe <stackprobes@gmail.com>
2020-08-14 17:41:02 +0900

    Mirage

--- Release.bat
+++ Release.bat
@@ -16,6 +16,7 @@ COPY /B G4Actor8301\G4Actor8301\bin\Release\DxLibDotNet.dll out
 
 C:\Factory\Tools\xcp.exe doc out
 C:\Factory\Tools\xcp.exe C:\Dev\Fairy\Donut3\doc out
+COPY /B AUTHORS out
 
 C:\Factory\SubTools\zip.exe /PE- /RVE- /G out G4Actor8301
 C:\Factory\Tools\summd5.exe /M out
