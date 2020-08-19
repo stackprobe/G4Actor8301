29267d04fbd174af8f2b7fc976ff17d71bb1b2d7
stackprobe <stackprobes@gmail.com>
2020-08-19 21:26:10 +0900

    Luminous

--- G4Actor8301/G4Actor8301/Common/DDMain.cs
+++ G4Actor8301/G4Actor8301/Common/DDMain.cs
@@ -135,10 +135,20 @@ namespace Charlotte.Common
 
 			// app > @ GameStart
 
-			DDFontRegister.Add(@"Fairy\Donut3\Font\Genkai-Mincho-font\genkai-mincho.ttf");
-			DDFontRegister.Add(@"Fairy\Donut3\Font\riitf\RiiT_F.otf");
+			// Font
+			{
+				//DDFontRegister.Add(@"Fairy\Donut3\Font\Genkai-Mincho-font\genkai-mincho.ttf");
+				//DDFontRegister.Add(@"Fairy\Donut3\Font\riitf\RiiT_F.otf");
+			}
 
 			Ground.I = new Ground();
+
+			// *.INIT
+			{
+				Charlotte.Games.MapTileManager.INIT();
+			}
+
+			MainWin.I.PostGameStart_G3();
 
 			// < app
 
